8a7ec09d169086a4bcb4a9678ab0ac857c3f36cd
root <muchemiwanjiru1m>
2024-01-24 12:08:31 -0800

    uuu

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,48 +1,94 @@
 #include <stdio.h>
-	#include <stdlib.h>
+#include <stdlib.h>
 
+/**
+ * main - the function printd the opcodes of itself.
+ * @argc: The number of commandline arguements
+ * @argv: An array of pointers to the commandline arguements
+ *
+ * Return: Always 0.
+ */
 
-	/**
-	 * main - prints its own opcodes
-	 * @argc: number of arguments
-	 * @argv: array of arguments
-	 *
-	 * Return: Always 0 (Success)
-	 */
-	int main(int argc, char *argv[])
+int main(int argc, char *argv[])
+{
+	int nums, i;
+	int (*address)(int, char **) = main;
+	unsigned char opcode;
+
+	if (argc != 2)
 	{
-		int bytes, i;
-		char *arr;
-
-
-		if (argc != 2)
-		{
-			printf("Error\n");
-			exit(1);
-		}
-
-
-		bytes = atoi(argv[1]);
-
-
-		if (bytes < 0)
-		{
-			printf("Error\n");
-			exit(2);
-		}
-
-
-		arr = (char *)main;
-
-
-		for (i = 0; i < bytes; i++)
-		{
-			if (i == bytes - 1)
-			{
-				printf("%02hhx\n", arr[i]);
-				break;
-			}
-			printf("%02hhx ", arr[i]);
-		}
-		return (0);
+		printf("Error\n");
+		exit(1);
 	}
+
+	nums = atoi(argv[1]);
+
+	if (nums < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < nums; i++)
+	{
+		opcode = *(unsigned char *)address;
+		printf("%.2x", opcode);
+
+		if (i == nums - 1)
+			continue;
+		printf(" ");
+
+		address++;
+	}
+
+	printf("\n");
+/*what is an opcode you ask?*/
+	return (0);
+}
+
+
+
+
+
+
+
+
+
+	
+
+	
+
+
+
+
+
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
